e85eea48e9eac16aa001a74f633a72307a1cff37
Tanishq Chugh <tanishqchugh85@gmail.com>
2021-12-20 05:05:19 +0530

    Dice on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,6 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +18,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int x = 5,y=4;
+  void dod(){
+    setState(() {
+      x = 1 + Random().nextInt(6);
+
+      y = 1 + Random().nextInt(6);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                dod();
+                print(x);
+              },
+              child: Image.asset('images/dice$x.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                dod();
+              },
+              child: Image.asset('images/dice$y.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
